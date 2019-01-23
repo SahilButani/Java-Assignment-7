a0c680b42281a3c5638dda871c9f9443ff4ee392
Sahil Butani <sahilbutani1760@gmail.com>
2019-01-23 16:38:15 +0530

    Update IOFiles.java

--- IOFiles.java
+++ IOFiles.java
@@ -1,28 +1,36 @@
 import java.util.*;
 import java.io.*;
-public class IOFiles{
-    public static void main(String[] args) throws Exception {
-        Map<Character, Integer> cc = new TreeMap<>();
-        for(char c='a';c<='z';c++){
-            cc.put(c,0);
-        }
+public class IOFiles
+{
+    public static void main(String[] args) throws Exception 
+    {
+        Map<Character, Integer> character_count = new TreeMap<>();
+
         File file = new File(args[0]); // input file
-        FileWriter fw = new FileWriter("/home/zadmin/output.txt"); // output file
+        FileWriter file_writer = new FileWriter("/home/zadmin/output.txt"); // output file
         Scanner sc= new Scanner(file);
-        while(sc.hasNextLine()) {
-            char[] c = sc.nextLine().toLowerCase().toCharArray();
-            for (Character ch : c) {
-                if (Character.isLetter(ch)) {
-                    cc.put(ch, cc.get(ch) + 1);
+        while(sc.hasNextLine())
+        {
+            char[] character_array = sc.nextLine().toLowerCase().toCharArray();
+            
+            for (Character ch : character_array) 
+            {
+                if (character_count.containsKey(ch))
+                {
+                    character_count.put(ch, character_count.get(ch) + 1);
                 }
+                else
+                character_count.put(ch,1);
             }
-
         }
-        for (Map.Entry<Character, Integer> entry : cc.entrySet()) {
+        //output is given to output.txt file
+        for (Map.Entry<Character, Integer> entry : character_count.entrySet()) 
+        {
             String s = entry.getKey() + " : " + entry.getValue() + "\n";
-            fw.write(s);
-
+            file_writer.write(s);
         }
-        fw.close();
+        
+        file_writer.close();
     }
+    
 }
